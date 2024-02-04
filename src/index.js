f1f38aadbbe664f8ef7de0106bdea91aa493c8f3
httpsyaseen <yaseenwalker1@gmail.com>
2024-02-05 01:14:37 +0500

    version 2.0.0

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 import App from "./App";
+import Appv1 from "./Appv1";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Appv1 />
   </React.StrictMode>
 );
